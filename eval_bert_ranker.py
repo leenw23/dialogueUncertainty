7fc39798c8a4b2ea62c6350857d7d10f5ef72fe3
ddehun <ddehun@kaist.ac.kr>
2021-04-02 13:21:36 +0900

    multiref accuracy

--- eval_bert_ranker.py
+++ eval_bert_ranker.py
@@ -41,34 +41,59 @@ def main(args):
     """
     STEP1. Simple evaluation using Accracy and F1 on DD testset.
     """
-    raw_dd_test = get_dd_corpus("test")
-    test_dataset = RankerDataset(raw_dd_test, tokenizer, "test", 128, UTTR_TOKEN)
-    softmax = torch.nn.Softmax(dim=1)
+    if False:
+        raw_dd_test = get_dd_corpus("test")
+        test_dataset = RankerDataset(raw_dd_test, tokenizer, "test", 128, UTTR_TOKEN)
+        softmax = torch.nn.Softmax(dim=1)
 
-    prediction_list, label_list = [], []
-    for idx in tqdm(range(len(test_dataset))):
-        with torch.no_grad():
-            sample = [el[idx] for el in test_dataset.feature]
-            ids, masks = [torch.unsqueeze(el, 0).to(device) for el in sample[:2]]
-            # 1 for positive and 0 for random
-            label_list.append(int(sample[2].numpy()))
-            prediction = softmax(model(ids, masks)[0]).cpu().numpy()[0]
-        assert len(prediction) == 2
-        prediction_list.append(prediction[1])
-        assert len(prediction_list) == len(label_list)
-    discrete_prediction_list = [1 if el >= 0.5 else 0 for el in prediction_list]
-    accuracy = accuracy_score(label_list, discrete_prediction_list)
-    f1 = f1_score(label_list, discrete_prediction_list)
-    print("Accuracy: {}".format(accuracy))
-    print("f1: {}".format(f1))
+        prediction_list, label_list = [], []
+        for idx in tqdm(range(len(test_dataset))):
+            with torch.no_grad():
+                sample = [el[idx] for el in test_dataset.feature]
+                ids, masks = [torch.unsqueeze(el, 0).to(device) for el in sample[:2]]
+                # 1 for positive and 0 for random
+                label_list.append(int(sample[2].numpy()))
+                prediction = softmax(model(ids, masks)[0]).cpu().numpy()[0]
+            assert len(prediction) == 2
+            prediction_list.append(prediction[1])
+            assert len(prediction_list) == len(label_list)
+        discrete_prediction_list = [1 if el >= 0.5 else 0 for el in prediction_list]
+        accuracy = accuracy_score(label_list, discrete_prediction_list)
+        f1 = f1_score(label_list, discrete_prediction_list)
+        print("Accuracy: {}".format(accuracy))
+        print("f1: {}".format(f1))
 
     """
     STEP2. Simple evaluation using Accracy and F1 on DD testset.
     """
     multiref_dd_dataset = get_dd_multiref_testset()
-    # TODO
-    # 여러 개의 proper responses에 대한 accuracy 정도를 먼저 봐보자.
-    # 그 다음에는 pos/rand-neg 비율이 1:(n-1) 이랑 n:0인 경우를 봐야 할까?
+    prediction_list = []
+    for sample in tqdm(multiref_dd_dataset):
+        context, responses = sample
+        assert isinstance(context, str)
+        assert isinstance(responses, list) and all(
+            [isinstance(el, str) for el in responses]
+        )
+        softmax = torch.nn.Softmax(dim=1)
+        for response in responses:
+            input_ids = tokenizer(
+                context,
+                text_pair=response,
+                max_length=128,
+                padding="max_length",
+                truncation=True,
+                return_tensors="pt",
+            )["input_ids"]
+            with torch.no_grad():
+                input_ids = input_ids.to(device)
+                prediction = float(softmax(model(input_ids)[0]).cpu().numpy()[0][1])
+                prediction_list.append(prediction)
+    discrete_prediction_list = [int(round(el)) for el in prediction_list]
+    accuracy = accuracy_score(
+        [1 for _ in range(len(discrete_prediction_list))],
+        discrete_prediction_list,
+    )
+    print("Accuracy: {}".format(accuracy))
 
 
 if __name__ == "__main__":
