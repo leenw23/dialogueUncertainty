78a7ced14358b751e4bbeedd108ea1a0ddab4140
ddehun <ddehun@kaist.ac.kr>
2021-04-05 14:34:00 +0900

    mcdropout for uncertaintly modeling

--- eval_uncertainty_with_dropout.py
+++ eval_uncertainty_with_dropout.py
@@ -1,3 +1,8 @@
+"""
+1. Single-candidate prediction을 할 때 MCDroput의 variance
+2. Multi-candidates prediction을 할 때의 entropy
+를 통해 uncertainty를 measure하는 스크립트 파일.
+"""
 import argparse
 import os
 
@@ -20,17 +25,70 @@ from tqdm import tqdm
 from transformers import BertConfig, BertForNextSentencePrediction, BertTokenizer
 
 from preprocess_dataset import get_dd_corpus, get_dd_multiref_testset
-from utils import (
-    RankerDataset,
-    dump_config,
-    get_uttr_token,
-    load_model,
-    set_random_seed,
-    write2tensorboard,
-)
+from utils import RankerDataset, get_uttr_token, load_model, set_random_seed
 
 
-def main(args):
+def modeling_mcdropout_uncertainty(model, ids, masks, softmax_op, num_forward_pass: int = 10):
+    """한 sample에 대한 예측 및 uncertainty value를 반환
+
+    Args:
+        model Deterministic BERT ranker
+        ids (torch.Tensor([1,seq_len]))
+        masks (torch.Tensor([1,seq_len]))
+        num_forward_pass (int, optional): 몇번 forward pass해서 분산을 구할건지. Defaults to 10.
+    """
+    model.train()
+    prediction_list = []
+    for forward_pass in range(num_forward_pass):
+        with torch.no_grad():
+            prediction_list.append(float(softmax_op(model(ids, masks)[0]).cpu().numpy()[0][1]))
+    return np.mean(prediction_list), np.var(prediction_list)
+
+
+def mcdrop_main(args):
+    set_random_seed(42)
+    device = torch.device("cuda")
+    tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+    UTTR_TOKEN = get_uttr_token()
+    special_tokens_dict = {"additional_special_tokens": [UTTR_TOKEN]}
+    tokenizer.add_special_tokens(special_tokens_dict)
+
+    model = BertForNextSentencePrediction.from_pretrained("bert-base-uncased")
+    model.resize_token_embeddings(len(tokenizer))
+    model = load_model(model, args.model_path, 0, len(tokenizer))
+    model.to(device)
+
+    raw_dd_test = get_dd_corpus("test")
+    test_dataset = RankerDataset(raw_dd_test, tokenizer, "test", 128, UTTR_TOKEN)
+    softmax = torch.nn.Softmax(dim=1)
+
+    prediction_list, uncertainty_list, label_list = [], [], []
+    for idx in tqdm(range(len(test_dataset))):
+        with torch.no_grad():
+            sample = [el[idx] for el in test_dataset.feature]
+            ids, masks = [torch.unsqueeze(el, 0).to(device) for el in sample[:2]]
+            prediction, uncertainty = modeling_mcdropout_uncertainty(
+                model,
+                ids,
+                masks,
+                softmax,
+            )
+            prediction_list.append(prediction)
+            uncertainty_list.append(uncertainty)
+            # 1 for positive and 0 for random
+            label_list.append(int(sample[2].numpy()))
+
+        assert len(prediction_list) == len(label_list) == len(uncertainty_list)
+    discrete_prediction_list = [1 if el >= 0.5 else 0 for el in prediction_list]
+    accuracy = accuracy_score(label_list, discrete_prediction_list)
+    f1 = f1_score(label_list, discrete_prediction_list)
+    calibration_error = cal.get_ece(prediction_list, label_list)
+    print("Accuracy: {}".format(accuracy))
+    print("f1: {}".format(f1))
+    print("ECE: {}".format(calibration_error))
+
+
+def multi_candidate_main(args):
     set_random_seed(42)
 
     dump_config(args)
@@ -48,34 +106,8 @@ def main(args):
     """
     STEP1. Simple evaluation using Accracy and F1 on DD testset.
     """
-    if False:
-        raw_dd_test = get_dd_corpus("test")
-        test_dataset = RankerDataset(raw_dd_test, tokenizer, "test", 128, UTTR_TOKEN)
-        softmax = torch.nn.Softmax(dim=1)
-
-        prediction_list, label_list = [], []
-        for idx in tqdm(range(len(test_dataset))):
-            with torch.no_grad():
-                sample = [el[idx] for el in test_dataset.feature]
-                ids, masks = [torch.unsqueeze(el, 0).to(device) for el in sample[:2]]
-                # 1 for positive and 0 for random
-                label_list.append(int(sample[2].numpy()))
-                prediction = softmax(model(ids, masks)[0]).cpu().numpy()[0]
-            assert len(prediction) == 2
-            prediction_list.append(prediction[1])
-            assert len(prediction_list) == len(label_list)
-        discrete_prediction_list = [1 if el >= 0.5 else 0 for el in prediction_list]
-        accuracy = accuracy_score(label_list, discrete_prediction_list)
-        f1 = f1_score(label_list, discrete_prediction_list)
-        calibration_error = cal.get_ece(prediction_list, label_list)
-        print("Accuracy: {}".format(accuracy))
-        print("f1: {}".format(f1))
-        print("ECE: {}".format(calibration_error))
-
-    """
-    STEP2. Simple evaluation using Accracy and F1 on DD testset.
-    """
     multiref_dd_dataset = get_dd_multiref_testset()
+
     prediction_list = []
     for sample in tqdm(multiref_dd_dataset):
         context, responses = sample
@@ -117,4 +149,5 @@ if __name__ == "__main__":
     args.exp_path = os.path.join(args.log_path, args.exp_name)
     args.model_path = os.path.join(args.exp_path, "model")
     args.board_path = os.path.join(args.exp_path, "board")
-    main(args)
+    parser.add_argument("--num_forward_pass", type=int, default=10)
+    mcdrop_main(args)
